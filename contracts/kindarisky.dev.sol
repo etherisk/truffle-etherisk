4c75328977d019b7d5e85acd87f88be0de82f187
Jasmine Kent <jk@codeartemis.com>
2016-02-27 20:54:11 +0100

    wip

--- contracts/kindarisky.dev.sol
+++ contracts/kindarisky.dev.sol
@@ -1,7 +1,6 @@
-import "std";
 
-contract KindaRisky is named("KindaRisky") {
-    
+contract KindaRisky {
+
     struct Country {
         uint id;
         uint nbNeighbors;
@@ -11,7 +10,7 @@ contract KindaRisky is named("KindaRisky") {
         uint bonus;
         uint lastGrowth;
     }
-    
+
     enum GameState {CREATED, IN_PROGRESS, DONE}
 
     struct Game {
@@ -25,15 +24,15 @@ contract KindaRisky is named("KindaRisky") {
         mapping(uint => Country) countries;
         mapping(uint => address) players;
     }
-    
+
     uint nbGames;
-    
+
     mapping(uint => Game) games;
-    
+
     function KindaRisky() {
         log0("Creating KindaRisky!");
     }
-    
+
     function getAvailableGames() public returns(int[10] result) {
         uint found = 0;
         uint i;
@@ -47,58 +46,58 @@ contract KindaRisky is named("KindaRisky") {
             }
         }
     }
-    
+
     function getNumberGames() constant returns(uint) { return nbGames; }
-    
+
     function getGameState(uint gameId) constant returns(uint) { return uint(games[gameId].state); }
-    
+
     function join(uint gameId) public returns (uint){
         log0("joining game");
         return addPlayerToGame(gameId,tx.origin);
-    } 
-    
+    }
+
     function startGame(uint gameId) {
         log0("game is starting!");
         assignPlayersToCountries(gameId);
     }
-    
+
     function addPlayerToGame(uint gameId, address player) returns (uint) {
         uint newplayer=games[gameId].nbPlayers;
         games[gameId].nbPlayers++;
         games[gameId].players[newplayer]=player;
         return games[gameId].nbPlayers;
     }
-    
+
     function createGame(uint numCountries) public {
         uint gameId = nbGames;
         nbGames++;
         Game newGame = games[gameId];
-        
+
         newGame.nbCountries = numCountries;
-        
+
         for (uint i; i<newGame.nbCountries; i++){
             newGame.countries[i].id = i;
             newGame.countries[i].numArmy = 4 + i;
             newGame.countries[i].bonus = 5;
             newGame.countries[i].lastGrowth =0;
         }
- 
+
         // create links between countries;
         linkNeighbors(gameId,0,1);
         linkNeighbors(gameId,2,3);
         linkNeighbors(gameId,1,3);
         linkNeighbors(gameId,3,0);
     }
-    
+
     function getNumberOfPlayers(uint gameId) returns (uint) {
         return games[gameId].nbPlayers;
     }
-    
+
     function getPlay(uint gameId, uint playId) returns (address) {
         return games[gameId].players[playId];
     }
-    
-    
+
+
     function assignPlayersToCountries(uint gameId) public { // pass gameID and players
         // for each player who was joined, assign player to country in a round-robin manner
         uint j=0;
@@ -111,18 +110,18 @@ contract KindaRisky is named("KindaRisky") {
             j++;
         }
     }
-    
+
     function linkNeighbors(uint gameId, uint id1, uint id2) {
         games[gameId].countries[id1].neighbors[id2] = games[gameId].countries[id2];
         games[gameId].countries[id2].neighbors[id1] = games[gameId].countries[id1];
         games[gameId].countries[id1].nbNeighbors++;
         games[gameId].countries[id2].nbNeighbors++;
     }
-    
+
     function joinWaitingRoom(uint roomNumber) public {
-        
+
     }
-    
+
     function is_neighbour(uint gameId, uint countryId1, uint countryId2) returns (bool) {
         //  mapping(uint => Country) neighbors;
         for (uint i=0; i<games[gameId].countries[countryId1].nbNeighbors; i++) {
@@ -131,12 +130,12 @@ contract KindaRisky is named("KindaRisky") {
         }
         return false;
     }
-    
+
     function move(uint gameId, uint countryId1, uint countryId2, uint nArmy) {
         Game currentGame = games[gameId];
         Country from = currentGame.countries[countryId1];
         Country to = currentGame.countries[countryId2];
-        
+
         if (tx.origin != from.owner)  {
             log0("doesn't own country 1");  // the caller doesn't own both countries
             return;
@@ -145,42 +144,42 @@ contract KindaRisky is named("KindaRisky") {
             log0("different owners");
             return;
         }
-        
+
         if (!is_neighbour(gameId, countryId1, countryId2)) {
-            log0("countries aren't neighbours"); 
+            log0("countries aren't neighbours");
             return;
         }                   // countries aren't neighbours
-        if (nArmy <= 0) { 
-            log0("army has size 0"); 
-            return; 
+        if (nArmy <= 0) {
+            log0("army has size 0");
+            return;
         }                                                                  // army has size 0
         if (nArmy >= from.numArmy) {nArmy = from.numArmy - 1;}                                                                  // not enough armys available in country 1
-        
+
         from.numArmy -= nArmy;
         to.numArmy += nArmy;
         log0("moved armies");
     }
-    
+
     function attack(uint gameId, uint countryId1, uint countryId2, uint nAttackers) {
         Game currentGame = games[gameId];
         Country from = currentGame.countries[countryId1];
         Country to = currentGame.countries[countryId2];
-        
+
         if (tx.origin != from.owner) {
-            log0("doesn't own attack country");  
+            log0("doesn't own attack country");
             return;
         }
 
         if (!is_neighbour(gameId, countryId1, countryId2)) {
-            log0("countries aren't neighbours"); 
+            log0("countries aren't neighbours");
             return;
         }
         if (nAttackers >= from.numArmy) {
             nAttackers = from.numArmy - 1;
-        } 
-        if (nAttackers <= 0) { 
-            log0("army has size 0"); 
-            return; 
+        }
+        if (nAttackers <= 0) {
+            log0("army has size 0");
+            return;
         }
 
         from.numArmy -= nAttackers;
@@ -201,7 +200,7 @@ contract KindaRisky is named("KindaRisky") {
         }
         log0('Attack completed');
     }
-    
+
     function getNumberOfArmies(uint gameId, uint countryId) returns (uint){
         return games[gameId].countries[countryId].numArmy;
     }
@@ -209,48 +208,48 @@ contract KindaRisky is named("KindaRisky") {
     function getCountryOwner(uint gameId, uint countryId) returns (address){
         return games[gameId].countries[countryId].owner;
     }
-        
+
     /*
-    
+
     */
     function takeCountryCheat(uint gameId, uint countryId){
         games[gameId].countries[countryId].owner = tx.origin;
-    }    
-    
+    }
+
      uint register=239847293742347;
-    
+
     function setSeed(uint seed){
         register=seed;
     }
-    
-     function getRandomNumber() public returns (uint) { 
+
+     function getRandomNumber() public returns (uint) {
          uint shiftOut=0;
          for (uint i=0; i<32; i++) {
-             register = ((((register /2**31) 
-                   ^ (register /2**6)   
-                   ^ (register /2**4)   
-                   ^ (register /2**2)   
-                   ^ (register /2**1)   
-                   ^ register)         
-                   & 0x0000001)  *2**31)               
-                   | (register /2**1);  
+             register = ((((register /2**31)
+                   ^ (register /2**6)
+                   ^ (register /2**4)
+                   ^ (register /2**2)
+                   ^ (register /2**1)
+                   ^ register)
+                   & 0x0000001)  *2**31)
+                   | (register /2**1);
              shiftOut=shiftOut|(register&0x1);
              shiftOut*=2;
          }
          return shiftOut;
      }
-     
-     function getRandomBit() public returns (bool) { 
-             register = ((((register /2**31) 
-                   ^ (register /2**6)   
-                   ^ (register /2**4)   
-                   ^ (register /2**2)   
-                   ^ (register /2**1)   
-                   ^ register)         
-                   & 0x0000001)  *2**31)               
-                   | (register /2**1);  
-             
+
+     function getRandomBit() public returns (bool) {
+             register = ((((register /2**31)
+                   ^ (register /2**6)
+                   ^ (register /2**4)
+                   ^ (register /2**2)
+                   ^ (register /2**1)
+                   ^ register)
+                   & 0x0000001)  *2**31)
+                   | (register /2**1);
+
          return (register&0x1)==1;
      }
-    
+
 }
