ff62a66b031221fb0e250d821ba099f068db147d
Jasmine Kent <jk@codeartemis.com>
2016-02-27 20:58:36 +0100

    add unity dev build, ick

---
+++ app/javascripts/app.js
@@ -0,0 +1,24 @@
+
+
+function getContract() {
+  return KindaRisky.deployed();
+}
+
+function getAvailableGames() {
+  return getContract().getAvailableGames.call();
+}
+
+window.onload = function() {
+
+  web3 = new Web3(new Web3.providers.HttpProvider("http://peer-1.ether.camp:8082/"));
+
+  getAvailableGames().then(function(games) {
+    console.log(games);
+    games.forEach(function(game) {
+      if (game != -1) {
+        var name = "Game " + game;
+        SendMessage('GameList', 'AddGame', name);
+      }
+    });
+  });
+}
