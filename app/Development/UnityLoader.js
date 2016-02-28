1da09b2e08b8f94f0d8591ed370fc8bedd1b9660
Jasmine Kent <jk@codeartemis.com>
2016-02-28 16:01:07 +0100

    new build

--- app/Development/UnityLoader.js
+++ app/Development/UnityLoader.js
@@ -3440,8 +3440,8 @@ Module.expectedDataFileDownloads++;
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
   
-      var REMOTE_PACKAGE_SIZE = 3413739;
-      var PACKAGE_UUID = '2006bb50-a09f-4d56-894f-2e009fcd012a';
+      var REMOTE_PACKAGE_SIZE = 3414413;
+      var PACKAGE_UUID = '1914d86c-916a-4ca3-8fea-a165f74998c1';
     
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -3545,12 +3545,12 @@ Module['FS_createPath']('/Managed/mono', '2.0', true, true);
       },
     };
 
-      new DataRequest(0, 77409, 0, 0).open('GET', '/data.unity3d');
-    new DataRequest(77409, 77581, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
-    new DataRequest(77581, 80701, 0, 0).open('GET', '/preserved_derived_types.xml');
-    new DataRequest(80701, 1889421, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
-    new DataRequest(1889421, 3386113, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(3386113, 3413739, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
+      new DataRequest(0, 77754, 0, 0).open('GET', '/data.unity3d');
+    new DataRequest(77754, 77987, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(77987, 81107, 0, 0).open('GET', '/preserved_derived_types.xml');
+    new DataRequest(81107, 1890095, 0, 0).open('GET', '/Il2CppData/Metadata/global-metadata.dat');
+    new DataRequest(1890095, 3386787, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(3386787, 3414413, 0, 0).open('GET', '/Managed/mono/2.0/machine.config');
 
     function processPackageData(arrayBuffer) {
       Module.finishedDataFileDownloads++;
--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -2,6 +2,22 @@ var riskContract;
 var account;
 var joinedGameId;
 
+/*
+  j Startup()
+  e getMyInProgressGames()
+  j FetchGameList()
+  u ClearGames()
+  e getAvailableGames()
+    e getNumberOfPlayers()
+    u AddGame()...
+    e amIMemberOf()
+  j CreateGame() OR JoinGame() (OR amIMemberOf() is true)
+  u SetJoinedGame()
+  (when we have enough players OR see state is IN_PROGRESS)
+  j StartGame()
+  e startGame()
+*/
+
 function getContract() {
   //return Kindarisky.at('d9e3996d5f4aece4d5878a2e2c8d986653e5532e');
   return Kindarisky.deployed();
@@ -11,39 +27,65 @@ function getAvailableGames() {
   return getContract().getAvailableGames.call();
 }
 
+function Startup() {
+  web3.eth.getAccounts(function(err, accs) {
+    account = accs[0];
+    getContract().getMyInProgressGames.call(account).then(function(games) {
+      for (var i = 0; i < games.length; ++i) {
+        if (games[i] != -1) {
+          SendMessage('GameListPanel', 'SetJoinedGame', '' + games[i]);
+          SendMessage('GameListPanel', 'LoadWorldScene');
+          return;
+        }
+      }
+      FetchGameList();
+    });
+  });
+}
+
 function FetchGameList() {
   SendMessage('GameListPanel', 'ClearGames', name);
   // This is called when the Unity app has finished starting up.
-  web3.eth.getAccounts(function(err, accs) {
-    account = accs[0];
-    getAvailableGames().then(function(games) {
-      console.log("Available games:");
-      console.log(games);
-      games.forEach(function(gameId) {
-        if (gameId != -1) {
-          getContract().getNumberOfPlayers.call(gameId).then(function(numPlayers) {
-            var name = gameId + " with " + numPlayers + " players";
-            console.log(name);
-            SendMessage('GameListPanel', 'AddGame', name);
-          });
-        }
-      });
+  getAvailableGames().then(function(games) {
+    console.log("Available games:");
+    console.log(games);
+    games.forEach(function(gameId) {
+      if (gameId != -1) {
+        getContract().getNumberOfPlayers.call(gameId).then(function(numPlayers) {
+          var name = gameId + " with " + numPlayers + " players";
+          console.log(name);
+          SendMessage('GameListPanel', 'AddGame', name);
+          getContract().amIMemberOf.call(gameId, account).then(function(isMember) {
+            console.log('member: ' + isMember);
+            if (isMember) {
+              SetJoinedGame(gameId);
+            }
+          })
+        });
+      }
     });
   });
 }
 
 function CreateGame() {
-  console.log("Create Game");
   getContract().createGame(3, 0, {from: account});
   FetchGameList();
 }
 
 function JoinGame(name) {
-  joinedGameId = parseInt(name);
-  console.log("Joining game " + joinedGameId);
+  SetJoinedGame(parseInt(name));
   getContract().join(joinedGameId, 0, {from:account});
-
   UpdatePlayerCount();
+}
+
+function SetJoinedGame(gameId) {
+  joinedGameId = gameId;
+  SendMessage('GameListPanel', 'SetJoinedGame', '' + joinedGameId);
+}
+
+function StartGame() {
+  getContract().startGame(joinedGameId, 0, {from:account});
+  SendMessage('GameListPanel', 'LoadWorldScene');
 }
 
 function UpdatePlayerCount() {
@@ -51,10 +93,6 @@ function UpdatePlayerCount() {
     var status = numPlayers + " joined out of 4";
     SendMessage('GameListPanel', 'SetStatus', status);
 
-    if (numPlayers == 1) {
-      SendMessage('GameListPanel', 'StartGame', joinedGameId)
-    } else {
-      setTimeout(UpdatePlayerCount, 3000);
-    }
+    setTimeout(UpdatePlayerCount, 3000);
   });
 }
